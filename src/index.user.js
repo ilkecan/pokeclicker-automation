83e410a9575de8e739e5024a5b489fa565dbad0f
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-08 18:04:20 +0300

    index.user: add empty user script

---
+++ src/index.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        pokeclicker-scripts
+// @namespace   ilkecan
+// @match       https://www.pokeclicker.com/
+// @grant       none
+// @icon        https://raw.githubusercontent.com/pokeclicker/pokeclicker/develop/src/assets/images/favicon.ico
+// @version     0.10.25
+// @author      ilkecan
+// @description key bindings & automation for various things
+// ==/UserScript==
+
+"use strict";
