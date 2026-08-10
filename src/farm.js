d54b982157085cb3dbcabff5934125922752cd9a
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-10 18:13:35 +0300

    farm: add `_harvestWitheringBerries`

--- src/farm.js
+++ src/farm.js
@@ -6,6 +6,28 @@ function _catchWanderers() {
   });
 }
 
+function _harvestWitheringBerries() {
+  App.game.farming.plotList.forEach((plot) => {
+    const shouldHarvest = ko.pureComputed(() => {
+      if (plot.berry === BerryType.None || plot.isSafeLocked || plot.stage() !== PlotStage.Berry) {
+        return false;
+      }
+
+      const growthMultiplier = App.game.farming.getGrowthMultiplier() * plot.getGrowthMultiplier();
+      if (growthMultiplier === 0) {
+        // freeze mulch
+        return false;
+      }
+
+      const remainingGrowth = plot.berryData.growthTime[PlotStage.Berry] - plot.age;
+      return remainingGrowth <= growthMultiplier;
+    });
+
+    _whenReady(shouldHarvest, () => App.game.farming.harvest(plot.index));
+  });
+}
+
 function automateFarm() {
   _catchWanderers();
+  _harvestWitheringBerries();
 }
