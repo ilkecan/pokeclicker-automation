28b109f69a09a56fd8d9b2a2a3abe5fabb3892b6
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-10 03:34:47 +0300

    underground: add `_sellTreasures`

--- src/underground.js
+++ src/underground.js
@@ -9,7 +9,21 @@ function _useSurvey() {
   _whenReady(tool.canUseTool, () => App.game.underground.tools.useTool(UndergroundToolType.Survey, 0, 0));
 }
 
-function automateUnderground() {
+function _dig() {
   _dischargeBattery();
   _useSurvey();
+}
+
+function _sellTreasures() {
+  const treasures = UndergroundItems.list.filter((item) => item.hasSellValue());
+  const treasuresToSell = treasures.filter((treasure) => ItemList[treasure.itemName].basePrice === Infinity); // exclude Everstone
+  for (const treasure of treasuresToSell) {
+    const canSell = ko.pureComputed(() => treasure.isUnlocked() && !treasure.sellLocked() && player.itemList[treasure.itemName]() > 0);
+    _whenReady(canSell, () => UndergroundTrading.quickSell(treasure));
+  }
+}
+
+function automateUnderground() {
+  _dig();
+  _sellTreasures();
 }
