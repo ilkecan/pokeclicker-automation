8160ece3765f697f0ba2da12ace98c271556d501
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-12 23:35:19 +0300

    quests: add `_startQuests`

--- src/quests.js
+++ src/quests.js
@@ -16,8 +16,56 @@ function _claimCompletedQuests() {
   });
 }
 
+const _questOrder = [
+  "CatchShiniesQuest",
+  "HatchEggsQuest",
+  "GainGemsQuest",
+  "MineLayersQuest",
+  "MineItemsQuest",
+
+  "GainMoneyQuest",
+  "DefeatPokemonsQuest",
+
+  "GainTokensQuest",
+  "CapturePokemonsQuest",
+  "CapturePokemonTypesQuest",
+  "UsePokeballQuest",
+
+  "DefeatGymQuest",
+  "DefeatDungeonQuest",
+
+  "HarvestBerriesQuest",
+  "GainFarmPointsQuest",
+
+  "ClearBattleFrontierQuest",
+  "CatchShadowsQuest",
+  "UseOakItemQuest",
+];
+
+const _questPriorityMapping = new Map(_questOrder.map((questType, priority) => [questType, priority]));
+
+function _questPriority(quest) {
+  return _questPriorityMapping.get(quest.constructor.name) ?? Infinity;
+}
+
+function _chooseQuestsToStart(quests, number) {
+  return quests.sort((a, b) => _questPriority(a) - _questPriority(b)).slice(0, number);
+}
+
+function _startQuests() {
+  const emptyQuestSlots = ko.pureComputed(() => App.game.quests.questSlots() - App.game.quests.currentQuests().length);
+  const unstartedQuests = ko.pureComputed(() => App.game.quests.incompleteQuests().filter((quest) => !quest.inProgress()));
+
+  _whenReady(ko.pureComputed(() => emptyQuestSlots() > 0 && unstartedQuests().length > 0), () => {
+    for (const quest of _chooseQuestsToStart(unstartedQuests(), emptyQuestSlots())) {
+      App.game.quests.beginQuest(quest.index);
+    }
+  });
+}
+
 function _automateQuests() {
   _claimCompletedQuests();
+  _startQuests();
 }
 
 function automateQuests() {
