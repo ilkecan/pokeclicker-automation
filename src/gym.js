2dab5c6d2b03d07462d911676dcb7f0200afaa91
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-09-01 00:13:42 +0300

    gym: add smart auto restart option

--- src/gym.js
+++ src/gym.js
@@ -3,6 +3,17 @@
 const gym = (() => {
   const SETTINGS_SECTION = "gym";
 
+  function gymNeedsRestart(gym) {
+    return _or([
+      App.game.quests.currentQuests().some((quest) => _and([
+        !quest.isCompleted(),
+        quest instanceof DefeatGymQuest,
+        quest.gymTown === gym.town,
+      ])),
+      gym.areaStatus().includes(areaStatus.missingAchievement),
+    ]);
+  }
+
   function patchStartGym(fn) {
     GymRunner.startGym = fn;
   }
@@ -32,11 +43,19 @@ const gym = (() => {
         patchStartGym(originalStartGym);
         originalStartGym = null;
       }
-    })
+    });
+
+    const stopAutoRestart = ko.pureComputed(() => _and([
+      !gymNeedsRestart(GymRunner.gymObservable()),
+      AutomationSettings.getValue(SETTINGS_SECTION, "smartAutoRestart"),
+      GymRunner.autoRestart(),
+    ]));
+    const stopSubscription = _whenReady(stopAutoRestart, () => GymRunner.autoRestart(false));
 
     return [
+      stopSubscription,
       subscription,
-      { dispose() { if (originalStartGym !== null) { patchStartGym(originalStartGym); } } }
+      { dispose() { if (originalStartGym !== null) { patchStartGym(originalStartGym); } } },
     ];
   }
 
